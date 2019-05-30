8a7227fd0dc522d5d3ed3988f6c730f0ce01df56
Engjames256 <engjames256@gmail.com>
2019-05-30 16:23:37 +0300

    Added Copyright to my profile

--- Norad_Class_Profile_Project/personal_portfolio_websites/pro3/Oyee James/Porfolio/JavaScript/main.js
+++ Norad_Class_Profile_Project/personal_portfolio_websites/pro3/Oyee James/Porfolio/JavaScript/main.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 //This function validates all the various inputs
 function validate (){
     
@@ -28,31 +26,4 @@ function validate (){
         }else{
         document.getElementById("comments").innerHTML="";
     }
-=======
-=======
->>>>>>> 4b5d27f741c391f76eb7a794c2fb5c1f0ad9754e
-function validate (){
-    var name = document.contactMe.name.value;
-    var email = document.contactMe.email.value;
-    var comments = document.contactMe.comments.value;
-    
-    if(name==""||!isNaN(name)){
-        document.getElementById("name").innerHTML="Required. Please provide a valid name*";
-        }else{
-        document.getElementById("name").innerHTML="";
-    }
-    if (/\S+@\S+/.test(email) == false|| email==""){
-        document.getElementById("email").innerHTML="Required. Please provide a valid email*";
-        }else{
-        document.getElementById("email").innerHTML="";
-    }
-    if(comments==""||!isNaN(comments)){
-        document.getElementById("comments").innerHTML="Required. Please leave a comment*"
-        }else{
-        document.getElementById("comments").innerHTML="";
-    }
-<<<<<<< HEAD
->>>>>>> 4b5d27f741c391f76eb7a794c2fb5c1f0ad9754e
-=======
->>>>>>> 4b5d27f741c391f76eb7a794c2fb5c1f0ad9754e
 }
